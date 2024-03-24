87769cc51735734d0c1e55b2fef51412b9083fc3
y0ssefmohamed <youssef.mohamed.ib@gmail.com>
2024-03-24 22:02:54 +0200

    Making openCoreDataContainer func to use it in the init instead of writing alot of code in init.

--- CoreData Todo/Core/Home/View Model/CoreDataManager.swift
+++ CoreData Todo/Core/Home/View Model/CoreDataManager.swift
@@ -8,6 +8,10 @@ class CoreDataManager
     let container: NSPersistentContainer
     init() {
         container = NSPersistentContainer(name: "TaskCoreData")
+        openCoreDataContainer()
+    }
+    
+    func openCoreDataContainer() {
         container.loadPersistentStores { description, error in
             if let error = error {
                 print("DEBUG: Error Loading Core Data Error -> \(error) \nIn init()")
